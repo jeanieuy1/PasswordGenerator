4a5730046b56b7fb9ff1e4e104d12a8264c85f2e
jeanieuy <89117286+jeanieuy@users.noreply.github.com>
2022-06-08 01:41:11 -0400

    final changes

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# PasswordGenerator
+## Description
+Homework #3 wherein students had to make sure that the site will generate a randomized password. Students had to make sure that the codebase follows the accessibility standards. 
 
+## Photos
+<img width="977" alt="Screen Shot 2022-06-08 at 1.37.24 AM" src=">
 
+## Deployed links
 https://jeanieuy1.github.io/PasswordGenerator/
 https://github.com/jeanieuy1/PasswordGenerator
--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-// Assignment Code
+// Assignment Code.
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  //Need to make to create a variable that can change
+  //Need to make to create a variable that can change.
   var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   //Listed out the potential characters for the password combinations.
   var passwordLength = 8;
-  //No less than 8 character passwords
+  //No less than 8 character passwords.
   var password = "";
-};
-
-for (var i = 0; i <= passwordLength; i++) {
-  var randomNumber = Math.floor(Math.random() * chars.length);
-  password += chars.substring(randomNumber, randomNumber + 1);
-};
-document.getElementById("password").value = password;
 
 
-//generates the password
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+  document.getElementById("password").value = password;
+}
+
+//generates the password.
 return password;
 
 // Write password to the #password input
@@ -29,5 +29,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//Add event listener to generate button
+generateBtn.addEventListener("click", writePassword); 
